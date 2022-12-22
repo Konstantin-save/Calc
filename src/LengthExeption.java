06d65d31e95d687c2b2b71801f261bf0c8b94c76
Konstantin <konstan.save@yandex.ru>
2022-12-22 22:33:58 +0300

    23

--- src/LengthExeption.java
+++
@@ -1,5 +0,0 @@
-public class LengthExeption extends Exception{      //создаем свое исключение,наследуется от класса Exception
-    public LengthExeption (String discription){     // определяем конструктор, который принимает в себя аргумент (строка)
-        super(discription);
-    }
-}
